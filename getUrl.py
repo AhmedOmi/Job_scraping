8480811fc9cdd6ff9ec25d98cb23fce98c680353
AhmedOmi <ahmedomarmiledi@gmail.com>
2021-04-17 16:37:35 +0100

    Add the first step to create scraping data for jobs

--- getUrl.py
+++ getUrl.py
@@ -1,6 +1,6 @@
 from urllib import request
 
-
+""" get urls from linkedin and indeed """
 def getUrl():
     Indeed = request.urlopen('https://www.indeed.com/jobs?q=machine+learning&jt=internship')
     linkedin = request.urlopen('https://www.linkedin.com/jobs/search/?f_JT=I&keywords=machine%20learning%20internship')
